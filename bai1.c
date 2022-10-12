25efde4040f6ff14c763c309337a4dfd38a0cf1e
DukLug <89517620+DukLug@users.noreply.github.com>
2022-10-12 07:25:16 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,15 @@
+/*
+	STT:
+	Ten:Nguyen Duc Luong
+	Chuong trinh nhap thong tin ten, ngay sinh. so dien thoai sinh vien
+*/
+#include<stdio.h>
+
+int main()
+{
+	printf("Ho va ten: Nguyen Duc Luong\n");
+	printf("Ngay sinh: 13/03/2003\n");
+	printf("So dien thoai: 0999999999");	
+	return 0;
+	
+}
