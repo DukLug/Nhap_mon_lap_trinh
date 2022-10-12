d2bec4c08571ceb13df1a3399ef30eb61c45291c
DukLug <89517620+DukLug@users.noreply.github.com>
2022-10-12 07:28:02 +0700

    Add files via upload

---
+++ Bai_2.c
@@ -0,0 +1,18 @@
+/*
+	STT:
+	Ten: Nguyen Duc Luong
+	Chuong trinh in ra hinh chu C.
+*/
+int main()
+{
+	printf("   ######\n");
+	printf(" ##      ##\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf(" ##      ##\n");
+	printf("   ######\n");
+	return 0;
+}
